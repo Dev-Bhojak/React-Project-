4434dbd5085722f2b41ef40973007c4d805ecd5f
Dev-Bhojak <devbhojak1@outlook.com>
2020-09-30 09:49:12 +1000

    minor changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ background: transparent;
 border: 0.05rem solid var(--lightBlue);
 border-color:${props => props.cart ? "var(--mainYellow)" : "var (--lightBlue)"};
 color: ${props => props.cart ? "var(--mainYellow)" : "var (--lightBlue)"};
-border-radius: 0.5rem;
+border-radius: 0.9rem;
 padding: 0.2rem 0.5reml;
 cursor:pointer;
 margin: 0.2rem 0.5rem 0.2rem 0;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button"
 
@@ -13,12 +12,12 @@ https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
                 <Link to="/">
-                    <img src={logo} alt="phone" className="navbar-brand" />
+                    <i className="fas fa-mobile-alt fa-3x"></i>
                 </Link>
                 <ul className="navbar-nav  align-items-center">
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
-                            Products
+                            Old Phones
                         </Link>
                     </li>
                 </ul>
